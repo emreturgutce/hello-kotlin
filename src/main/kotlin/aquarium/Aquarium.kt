96dd93cafd6427ccaea69fa8a8bcae475a61e4c7
Emre Turgut <emreturgutcse@hotmail.com>
2021-10-02 13:48:21 +0300

    inheritance

--- src/main/kotlin/aquarium/Aquarium.kt
+++ src/main/kotlin/aquarium/Aquarium.kt
@@ -1,18 +1,31 @@
 package aquarium
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+import kotlin.math.PI
 
-    var volume: Int
+// open = subclassable
+
+open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+
+    open var volume: Int
         get() = width * height * length / 1000
         set(value) {
             height = (value * 1000) / (width * height)
         }
 
-    var water = volume * 0.9
+    open var water = volume * 0.9
 
     constructor(numberOfFish: Int) : this() {
         val water = numberOfFish * 2000
         val tank = water + water * .1
         height = (tank / (length * width)).toInt()
     }
+}
+
+class TowerTank(): Aquarium() {
+
+    override var water = volume * .8
+
+    override var volume: Int
+        get() = (width * height * length / 1000 * PI).toInt()
+        set(value) { height = (volume * 1000) / (width * length) }
 }
