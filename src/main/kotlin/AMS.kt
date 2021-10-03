be0f1a5398371c863b37672bbca4817de3d87a82
Emre Turgut <emreturgutcse@hotmail.com>
2021-10-03 20:40:15 +0300

    pairs

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -1,23 +1,5 @@
 fun main(args: Array<String>) {
-    for (i in 1..10) {
-        println(getFortuneCookie())
-    }
+    val equipment = "fishnet" to "catching fish"
+
+    println(equipment.first + equipment.second)
 }
-
-fun getFortuneCookie(): String {
-    val fortunes = listOf(
-        "You will have a great day!",
-        "Things will go well for you today.",
-        "Enjoy a wonderful day of success.",
-        "Be humble and all will turn out well."
-    )
-
-    print("Enter your birthday:")
-
-    val birthday = readLine()?.toIntOrNull() ?: 0
-
-    val idx = birthday.rem(fortunes.size)
-
-    return fortunes[idx]
-}
-
