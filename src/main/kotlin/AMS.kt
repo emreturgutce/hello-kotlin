c036b148024ababa1f16794b43d65518d6465a8f
Emre Turgut <emreturgutcse@hotmail.com>
2021-10-03 20:49:13 +0300

    collections

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -1,5 +1,23 @@
 fun main(args: Array<String>) {
-    val equipment = "fishnet" to "catching fish"
+    val symptoms = mutableListOf("white spots", "red spots", "not eating", "bloated", "belly up")
 
-    println(equipment.first + equipment.second)
+    symptoms.add("white fungus")
+    symptoms.remove("white fungus")
+
+    symptoms.contains("red")
+    symptoms.contains("red spots")
+
+    println(symptoms.subList(4, symptoms.size))
+
+    listOf(1, 5, 3).sum()
+
+    listOf("a", "b", "cc").sumOf { it.length }
+
+    val cures = mapOf("white spots" to "Ich", "red sores" to "hole disease")
+
+    cures["white spots"]
+
+    val inventory = mutableMapOf("fish net" to 1)
+
+    inventory["tank scrubber"] = 3
 }
